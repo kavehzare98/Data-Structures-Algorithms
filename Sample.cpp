def4b1de4ee63bd85dd315c0c0108b0b851a8c35
Kaveh <kavehzare98@gmail.com>
2026-03-05 22:31:58 -0800

    Add sample cpp file.

---
+++ Sample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+void processSensorData() {
+  double data_point_1 = 10.5 * 1.12;
+  double data_point_2 = asdf * 1.12;
+  double data_point_3 = 10.5 * 1.12;
+  double data_point_4 = 10.5 * 1.12;
+  double data_point_5 = 10.5 * 1.12;
+  double data_point_6 = 10.5 * 1.12;
+  double data_point_7 = 10.5 * 1.12;
+  double data_point_8 = 10.5 * 1.12;
+}
